d029ca9946614351f21a6c8cd82765920c88c901
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2022-12-19 10:47:49 +0100

    Update version v4.3.6 and v3.7.4

--- src/script.js
+++ src/script.js
@@ -2,11 +2,12 @@
  * Load docs
  */
 $("section[data]").each(function (_index, section) {
-  var request = new XMLHttpRequest();
+  let request = new XMLHttpRequest();
   request.open("GET", $(section).attr("data"), true);
   request.send(null);
   request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
+      // file deepcode ignore DOMXSS: is desired to load the html and the js from the source file.
       $(section).html(request.responseText);
     }
     if (request.readyState === 4 && request.status != 200) {
@@ -25,48 +26,52 @@ $("section[data]").each(function (_index, section) {
  */
 $().ready(function () {
   //Blinking logo
-  var logo_img = $(".img-toggle img");
+  let logo_img = $(".img-toggle img");
   setInterval(function () {
     logo_img.toggleClass("invisible");
   }, 2000);
 
   //Version listener
-  $('#version').on('change',function(){
+  $("#version").on("change", function () {
     window.location.href = $(this).val();
   });
 
   // Wait for load
   setTimeout(function () {
     // Anchor root mapping
-    $('a[root]').each(function(_index, anchor){
-      $(anchor).attr('href',ROOT + $(anchor).attr('root'));
+    $("a[root]").each(function (_index, anchor) {
+      $(anchor).attr("href", ROOT + $(anchor).attr("root"));
     });
-    
+
     // Add table of contents
     $("#toc").html("");
     Toc.init({
       $nav: $("#toc"),
     });
     const _scrollSpy = new bootstrap.ScrollSpy(document.body, {
-      target: '#toc'
+      target: "#toc",
     });
 
     // format examples
     $(".example:not(.skip)").each(function () {
       // fetch & encode html
-      var html = $("<div>").text($(this).html()).html();
+      let html = $("<div>").text($(this).html()).html();
       // find number of space/tabs on first line (minus line break)
-      var count = html.match(/^(\s+)/)[0].length - 1;
+      let count = html.match(/^(\s+)/)[0].length - 1;
       // replace tabs/spaces on each lines with
-      var regex = new RegExp("\\n\\s{" + count + "}", "g");
-      var code = html.replace(regex, "\n").replace(/\t/g, "  ").trim();
+      let regex = new RegExp("\\n\\s{" + count + "}", "g");
+      let code = html.replace(regex, "\n").replace(/\t/g, "  ").trim();
       // other cleanup
       code = code.replace(/=""/g, "");
       // add code block to dom
       $(this).after(
         $("<pre>").append(
           $('<code class="highlight">')
-            .addClass("language-" + $(this).attr("code-lang"))
+            .addClass(
+              $(this).attr("code-lang")
+                ? "language-" + $(this).attr("code-lang")
+                : "language-html"
+            )
             .html(code)
         )
       );
@@ -81,37 +86,37 @@ $().ready(function () {
   }, 2000);
 });
 
-var Demo = function () {};
+class Demo {
+  init = function (selector) {
+    $(selector).bootstrapToggle(selector);
+  };
+  destroy = function (selector) {
+    $(selector).bootstrapToggle("destroy");
+  };
+  on = function (selector) {
+    $(selector).bootstrapToggle("on");
+  };
+  off = function (selector) {
+    $(selector).bootstrapToggle("off");
+  };
+  toggle = function (selector) {
+    $(selector).bootstrapToggle("toggle");
+  };
+  enable = function (selector) {
+    $(selector).bootstrapToggle("enable");
+  };
+  disable = function (selector) {
+    $(selector).bootstrapToggle("disable");
+  };
+  readonly = function (selector) {
+    $(selector).bootstrapToggle("readonly");
+  };
+  indeterminate = function (selector) {
+    $(selector).bootstrapToggle("indeterminate");
+  };
+  determinate = function (selector) {
+    $(selector).bootstrapToggle("determinate");
+  };
+}
 
-Demo.prototype.init = function (selector) {
-  $(selector).bootstrapToggle(selector);
-};
-Demo.prototype.destroy = function (selector) {
-  $(selector).bootstrapToggle("destroy");
-};
-Demo.prototype.on = function (selector) {
-  $(selector).bootstrapToggle("on");
-};
-Demo.prototype.off = function (selector) {
-  $(selector).bootstrapToggle("off");
-};
-Demo.prototype.toggle = function (selector) {
-  $(selector).bootstrapToggle("toggle");
-};
-Demo.prototype.enable = function (selector) {
-  $(selector).bootstrapToggle("enable");
-};
-Demo.prototype.disable = function (selector) {
-  $(selector).bootstrapToggle("disable");
-};
-Demo.prototype.readonly = function (selector) {
-  $(selector).bootstrapToggle("readonly");
-};
-Demo.prototype.indeterminate = function (selector) {
-  $(selector).bootstrapToggle("indeterminate");
-};
-Demo.prototype.determinate = function (selector) {
-  $(selector).bootstrapToggle("determinate");
-};
-
-demo = new Demo();
+const demo = new Demo();
